f1b7426d4fd071fcbd831267e865f850a6e481fd
Gilmar Milandri <gilmar.milandri@ufms.br>
2025-11-24 11:36:21 -0400

    Criando contexto de autenticação

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './App.tsx'
 import { RouterProvider } from 'react-router'
+import AuthProvider from './contexts/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router}/>
+    <AuthProvider>
+        <RouterProvider router={router}/>
+    </AuthProvider>
 )
