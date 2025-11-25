3303b55cf80086ede088924eea3c45ba8c119f84
Gilmar Milandri <gilmar.milandri@ufms.br>
2025-11-25 11:14:05 -0400

    Buscando locais na página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,56 @@
 import { Container } from "../../components/container"
+import { useState, useEffect } from "react"
+import { collection, query, getDocs, orderBy } from "firebase/firestore"
+import { db } from "../../services/firebaseConnection"
+import { Link } from "react-router"
+
+interface LocationsProps{
+  id: string;
+  nome: string;
+  horario: string;
+  valor: string;
+  bairro: string;
+  images: LocalImageProps[];
+}
+
+interface LocalImageProps{
+  nome: string;
+  uid: string;
+  url: string;
+}
+
 
 export function Home() {
+  const [locations, setLocations] = useState<LocationsProps[]>([])
+
+  useEffect(() => {
+    function loadLocations(){
+      const locationsRef = collection(db, "locations")
+      const queryRef = query (locationsRef, orderBy("created", "desc"))
+   
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let listlocations = [] as LocationsProps[];
+
+        snapshot.forEach(doc => {
+          listlocations.push({
+            id: doc.id,
+            bairro: doc.data().bairro,
+            nome: doc.data().nome,
+            horario: doc.data().horario,
+            valor: doc.data().valor,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          })
+        })
+
+        setLocations(listlocations)
+      })
+    }
+
+    loadLocations();
+  }, [])
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full  max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -20,24 +70,28 @@ export function Home() {
       </h1>
       
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg">
+      {locations.map( location => (
+        <Link to={`/location/${location.id}`} key={location.id}>
+          <section className="w-full bg-white rounded-lg">
             <img 
                 className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                src="https://cdn6.campograndenews.com.br/uploads/noticias/2022/03/21/66fbc44f674c3e76b45b72c1923b24a05d51b76f.jpg"
+                src={location.images[0].url}
                 alt="Parque das Nações Indigenas em Campo Grande - MS."
             />
-            <p className="font-bold mt-1 mb-2 px-2">Parque das Nações Indigenas</p>
+            <p className="font-bold mt-1 mb-2 px-2">{location.nome}</p>
             <div className="flex flex-col px-2">
-                <span className="text-zinc-700 mb-6 ">Horário: 08:00 as 23:00</span>
-                <strong className="text-black font-medium text-xl">Entrada Gratuita!</strong>
+                <span className="text-zinc-700 mb-6 ">Horário: {location.horario}</span>
+                <strong className="text-black font-medium text-xl">Entrada: {location.valor}</strong>
             </div>
             <div className="w-full h-px bg-slate-200 my-2"></div>
             <div className="px-2 pb-2">
                 <span className="text-black">
-                    Parque dos Poderes
+                    {location.bairro}
                 </span>
             </div>
-        </section>
+          </section>
+        </Link>
+      ))}
         
       </main>
     </Container>
